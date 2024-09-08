7615df4683cec574256202525c8ad5cd9285e890
chri-saxe <154531461+chri-saxe@users.noreply.github.com>
2024-09-08 18:36:43 -0400

    Bug fix
    www.reddit url containing "old" used old.reddit format.

--- content/content.js
+++ content/content.js
@@ -9,7 +9,7 @@ console.log("(REO) Connexion");
 chrome.runtime.sendMessage({action: "getTabUrl"}, function(response) {
     const url = response.url;
 
-    if (!url.includes("old") && !url.includes("/comments/")) { //subreddit pages
+    if (!url.includes("old.") && !url.includes("/comments/")) { //subreddit pages
         const to_remove_sub_pages = [`[id="left-sidebar-container"]`, `[id="right-sidebar-container"]`, `[class="flex w-100 xs:w-auto mt-xs xs\\:mt-0"]`, ".promotedlink", `div[slot="post-media-container"]`, "nav", `div[class="@container"]`]; // 3 = publier un post, last = bannière
         
         for (i of to_remove_sub_pages) {for (j of document.querySelectorAll(i)) {j.remove()}};
@@ -20,7 +20,7 @@ chrome.runtime.sendMessage({action: "getTabUrl"}, function(response) {
             for (i of [`div[slot="post-media-container"]`, ".promotedlink"]) {for (i of document.querySelectorAll(i)) {i.remove()}};
         }, 3000);
 
-    } else if (url.includes("/comments/") && !url.includes("old")) { // comment pages
+    } else if (!url.includes("old.") && url.includes("/comments/")) { // comment pages
         let to_remove = ["#right-sidebar-container", "#left-sidebar-container", "comment-body-header", "nav", ".promotedlink", "#comment-tree > shreddit-comment:nth-child(n+6)"];
         
         setTimeout(() => { // Délai le temps que les premiers messages se chargent
@@ -32,9 +32,9 @@ chrome.runtime.sendMessage({action: "getTabUrl"}, function(response) {
             window.scrollTo({top: 0, left: 0});
             for (item of document.querySelectorAll("#comment-tree > shreddit-comment:nth-child(n+6)")) {item.remove()}; // Tous les messages suivants sauf les 6 premiers
             document.querySelector(`faceplate-tracker[noun="load_more_comments"]`).remove(); // Boutton afficher plus de commentaires
-        }, 2500);
+        }, 1500);
         
-    } else if (url.includes("old") && !url.includes("/comments/")) { //old subreddit pages
+    } else if (url.includes("old.") && !url.includes("/comments/")) { //old subreddit pages
         const to_remove_sub_pages = [`section`, "#header-bottom-left", ".side", "#header", ".infobar-toaster-container", ".promotedlink"];
         
         for (i of to_remove_sub_pages) {for (j of document.querySelectorAll(i)) {j.remove()}};
@@ -42,7 +42,7 @@ chrome.runtime.sendMessage({action: "getTabUrl"}, function(response) {
         document.body.setAttribute("style", "max-width: 1500px; margin: auto; background-color: #F7FAFE; box-shadow: 0 0 195px -13px rgba(0,0,0,0.5)") ;
         document.body.style.overflow = "hidden";
 
-    } else if (url.includes("old") && url.includes("/comments/")) { //old comment pages
+    } else if (url.includes("old.") && url.includes("/comments/")) { //old comment pages
         const to_remove_sub_pages = [`section`, "#header-bottom-left", ".side", "#header", ".infobar-toaster-container", ".title-button", ".promotedlink", ".morechildren", ".debuginfo", ".footer-parent", `div[class="sitetable nestedlisting"] > .thing.comment:nth-child(n+10)`];
         
         for (i of to_remove_sub_pages) {for (j of document.querySelectorAll(i)) {j.remove()}};
