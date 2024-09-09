533a2e108070ea36e1020ccde701a2658e3bf8d6
chri-saxe <154531461+chri-saxe@users.noreply.github.com>
2024-09-08 20:51:22 -0400

    update

--- content/content.js
+++ content/content.js
@@ -2,7 +2,7 @@ console.log("(REO) Connexion");
 
 // const reo_is_active = true;
 // chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//     reo_is_active = (request.action === "toggle_reo") ? !reo_is_active : reo_is_active;
+//     reo_is_active = (request.action === "toggle_reo") ? !reo_is_active : reo_is_active; // 
 // });
 
 // if (reo_is_active) {
@@ -10,7 +10,7 @@ chrome.runtime.sendMessage({action: "getTabUrl"}, function(response) {
     const url = response.url;
 
     if (!url.includes("old.") && !url.includes("/comments/")) { //subreddit pages
-        const to_remove = [`[id="left-sidebar-container"]`, `[id="right-sidebar-container"]`, `[class="flex w-100 xs:w-auto mt-xs xs\\:mt-0"]`, ".promotedlink", `div[slot="post-media-container"]`, "nav", `div[class="@container"]`]; // 3 = publier un post, last = bannière
+        const to_remove = [`[id="left-sidebar-container"]`, `[id="right-sidebar-container"]`, `[class="flex w-100 xs:w-auto mt-xs xs\\:mt-0"]`, ".promotedlink", `div[slot="post-media-container"]`, "nav", `div[class="@container"]`, `[sort-event="layout-view-change"]`]; // 3 = publier un post, last = bannière
         
         for (i of to_remove) {for (j of document.querySelectorAll(i)) {j.remove()}};
         window.scrollTo({top: 0, left: 0});
@@ -21,7 +21,7 @@ chrome.runtime.sendMessage({action: "getTabUrl"}, function(response) {
         }, 3000);
 
     } else if (!url.includes("old.") && url.includes("/comments/")) { // comment pages
-        let to_remove = ["#right-sidebar-container", "#left-sidebar-container", "comment-body-header", "nav", ".promotedlink", "#comment-tree > shreddit-comment:nth-child(n+6)"];
+        let to_remove = ["#right-sidebar-container", "#left-sidebar-container", "comment-body-header", "nav", ".promotedlink", "shreddit-comment-tree-ad", `#comment-tree > [depth="0"]:nth-child(n+6)`]; // #comment-tree > [depth="0"]:nth-child(n+6) le premier commentaire comment à n+2, donc là ça fait 4
         
         setTimeout(() => { // Délai le temps que les premiers messages se chargent
             window.scrollTo({top: 15000, left: 0, behavior: 'smooth'});
@@ -30,7 +30,9 @@ chrome.runtime.sendMessage({action: "getTabUrl"}, function(response) {
         
         setTimeout(() => { // Délai le temps que les nouveaux messages se chargent
             window.scrollTo({top: 0, left: 0});
-            for (item of document.querySelectorAll("#comment-tree > shreddit-comment:nth-child(n+6)")) {item.remove()}; // Tous les messages suivants sauf les 6 premiers
+            for (item of document.querySelectorAll(`#comment-tree > [depth="0"]:nth-child(n+6)`)) {item.remove()}; // Tous les messages suivants sauf les 6 premiers
+            for (i of document.querySelectorAll(".promotedlink")) {i.remove()}
+            for (i of document.querySelectorAll("shreddit-comment-tree-ad")) {i.remove()}
             document.querySelector(`faceplate-tracker[noun="load_more_comments"]`).remove(); // Boutton afficher plus de commentaires
         }, 1500);
         
